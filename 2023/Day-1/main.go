da23a8c03557c1a409abee307619d7e8f249f432
lanetb <lanetb@dukes.jmu.edu>
2023-12-01 13:47:56 -0500

    Completed Day-1 Part 2

--- 2023/Day-1/main.go
+++ 2023/Day-1/main.go
@@ -9,6 +9,16 @@ import (
     "strconv"
 )
 
+var ONE string = "one"
+var TWO string = "two"
+var THREE string = "three"
+var FOUR string = "four"
+var FIVE string = "five"
+var SIX string = "six"
+var SEVEN string = "seven"
+var EIGHT string = "eight"
+var NINE string = "nine"
+
 func main() {
 	file, err := os.Open("input.txt")
     if err != nil {
@@ -20,18 +30,159 @@ func main() {
 	scanner := bufio.NewScanner(file)
     
     for scanner.Scan() {
+        fmt.Printf("%s ", scanner.Text())
         digit := ""
-        for _, c := range scanner.Text() {
+        forloop:for i, c := range scanner.Text() {
             if unicode.IsDigit(c){
                 digit = digit + string(c)
                 break
+            } else{
+                switch byte(c) {
+                    case ONE[0]:
+                        if len(scanner.Text()[i:]) >= 3 {
+                            if scanner.Text()[i:i+3] == ONE {
+                                digit = digit + "1"
+                                break forloop
+                            }
+                        } 
+                    case TWO[0]: 
+                        if len(scanner.Text()[i:]) >= 3 {
+                            if scanner.Text()[i:i+3] == TWO {
+                                digit = digit + "2"
+                                break forloop
+                            }
+                        }
+                        fallthrough
+                    case THREE[0]:
+                        if len(scanner.Text()[i:]) >= 5 {
+                            if scanner.Text()[i:i+5] == THREE {
+                                digit = digit + "3"
+                                break forloop
+                            }
+                        } 
+                    case FOUR[0]: 
+                        if len(scanner.Text()[i:]) >= 4 {
+                            if scanner.Text()[i:i+4] == FOUR {
+                                digit = digit + "4"
+                                break forloop
+                            } 
+                        }
+                        fallthrough
+                    case FIVE[0]:
+                        if len(scanner.Text()[i:]) >= 4 {
+                            if scanner.Text()[i:i+4] == FIVE {
+                                digit = digit + "5"
+                                break forloop
+                            }
+                        } 
+                    case SIX[0]: 
+                        if len(scanner.Text()[i:]) >= 3 {
+                            if scanner.Text()[i:i+3] == SIX {
+                                digit = digit + "6"
+                                break forloop
+                            } 
+                        } 
+                        fallthrough
+                    case SEVEN[0]:
+                        if len(scanner.Text()[i:]) >= 5 {
+                            if scanner.Text()[i:i+5] == SEVEN {
+                                digit = digit + "7"
+                                break forloop
+                            }
+                        }
+                    case EIGHT[0]:
+                        if len(scanner.Text()[i:]) >= 5 {
+                            if scanner.Text()[i:i+5] == EIGHT {
+                                digit = digit + "8"
+                                break forloop
+                            }
+                        }
+                    case NINE[0]:
+                        if len(scanner.Text()[i:]) >= 4 {
+                            if scanner.Text()[i:i+4] == NINE {
+                                digit = digit + "9"
+                                break forloop
+                            }
+                        }
+                }
             }
         }
-        for i := len(scanner.Text()) - 1; i >= 0; i-- {
+
+        forloop2:for i := len(scanner.Text()) - 1; i >= 0; i-- {
             c := scanner.Text()[i]
             if unicode.IsDigit(rune(c)) {
                 digit = digit + string(c)
                 break
+            } else {
+                switch byte(c) {
+                    case ONE[2]: 
+                        if len(scanner.Text()[:i]) >= 3 {
+                            if scanner.Text()[i-2:i+1] == ONE {
+                                digit = digit + "1"
+                                break forloop2
+                            }
+                        } 
+                        fallthrough
+                    case THREE[4]:
+                        if len(scanner.Text()[:i]) >= 5 {
+                            if scanner.Text()[i-4:i+1] == THREE {
+                                digit = digit + "3"
+                                break forloop2
+                            }
+                        }
+                        fallthrough
+                    case FIVE[3]:
+                        if len(scanner.Text()[:i]) >= 4 {
+                            if scanner.Text()[i-3:i+1] == FIVE {
+                                digit = digit + "5"
+                                break forloop2
+                            }
+                        }
+                        fallthrough
+                    case NINE[3]:
+                        if len(scanner.Text()[:i]) >= 4 {
+                            if scanner.Text()[i-3:i+1] == NINE {
+                                digit = digit + "9"
+                                break forloop2
+                            }
+                        }
+
+                    case TWO[2]:
+                        if len(scanner.Text()[:i]) >= 2 {
+                            if scanner.Text()[i-2:i+1] == TWO {
+                                digit = digit + "2"
+                                break forloop2
+                            }
+                        }  
+                    case FOUR[3]: 
+                        if len(scanner.Text()[:i]) >= 4 {
+                            if scanner.Text()[i-3:i+1] == FOUR {
+                                digit = digit + "4"
+                                break forloop2
+                            } 
+                        } 
+                    case SIX[2]: 
+                        if len(scanner.Text()[:i]) >= 3 {
+                            if scanner.Text()[i-2:i+1] == SIX {
+                                digit = digit + "6"
+                                break forloop2
+                            } 
+                        } 
+                    case SEVEN[4]:
+                        if len(scanner.Text()[:i]) >= 5 {
+                            if scanner.Text()[i-4:i+1] == SEVEN {
+                                digit = digit + "7"
+                                break forloop2
+                            }
+                        }
+                    case EIGHT[4]:
+                        if len(scanner.Text()[:i]) >= 5 {
+                            if scanner.Text()[i-4:i+1] == EIGHT {
+                                digit = digit + "8"
+                                break forloop2
+                            }
+                        }
+                    }
             }
         }
         fmt.Println(digit)
@@ -43,6 +194,6 @@ func main() {
     if err := scanner.Err(); err != nil {
         fmt.Println(err)
     }
-    // make a buffer to keep chunks that are read
+
 	
 }
