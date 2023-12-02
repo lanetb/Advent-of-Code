22e328358f5856113ad03f711c39e872e4343628
lanetb <lanetb@dukes.jmu.edu>
2023-12-02 13:57:37 -0500

    Day-2 Part 2

--- 2023/Day-2/main.go
+++ 2023/Day-2/main.go
@@ -18,9 +18,9 @@ func main() {
 		blue  int
 	}
 
-	GAMEREGEX, _ := regexp.Compile("Game [0-9]+")
-	AMOUNTREGEX, _ := regexp.Compile("[1-9]+ [a-z]+")
-	currentBag := Bag{red: 12, green: 13, blue: 14}
+	//GAMEREGEX, _ := regexp.Compile("Game [0-9]+")
+	AMOUNTREGEX, _ := regexp.Compile("[0-9]+ [a-z]+")
+	//currentBag := Bag{red: 12, green: 13, blue: 14}
 
 	file, err := os.Open("input.txt")
     if err != nil {
@@ -32,9 +32,10 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	total := 0
     for scanner.Scan() {
-		valid_game := true
-		gamenumber := strings.Split(string(GAMEREGEX.Find(scanner.Bytes())), " ")[1]
+		//valid_game := true
+		//gamenumber := strings.Split(string(GAMEREGEX.Find(scanner.Bytes())), " ")[1]
 		amounts := AMOUNTREGEX.FindAll(scanner.Bytes(), -1)
+		minred, mingreen, minblue := 0, 0, 0
 
 		for _, item := range amounts {
 			items := strings.Split(string(item), " ")
@@ -42,29 +43,30 @@ func main() {
 			case "red":
 				itemstr := string(items[0])
 				i, _:= strconv.Atoi(itemstr)
-				if i > currentBag.red {
-					valid_game = false
+				fmt.Printf("Red: %d\n", i)
+				if i > minred {
+					minred = i
 				}
 			case "green":
 				itemstr := string(items[0])
+				fmt.Printf("Green: %s\n", itemstr)
 				i, _:= strconv.Atoi(itemstr)
-				if i > currentBag.green {
-					valid_game = false
+				if i > mingreen {
+					mingreen = i
 				}
 			case "blue":
 				itemstr := string(items[0])
+				fmt.Printf("Blue: %s\n", itemstr)
 				i, _:= strconv.Atoi(itemstr)
-				if i > currentBag.blue {
-					valid_game = false
+				if i > minblue {
+					minblue = i
 				}
 			}
 		}
-
-		if valid_game {
-			num, _ := strconv.Atoi(gamenumber)
-			total += num
-		}
-
+		fmt.Printf("Red: %d, Green: %d, Blue: %d\n", minred, mingreen, minblue)
+		temptotal := minred * mingreen * minblue
+		fmt.Printf("TempTotal: %d\n", temptotal)
+		total += temptotal
 	}
 
 	fmt.Printf("Total: %d\n", total)
